1a5e50dbf1f8574b8547717f2df06f65bffc209d
hb432 <hb432@protonmail.com>
2020-11-13 21:44:36 -0500

    add more globals to index.d.ts (was needed)

--- dict/index.d.ts
+++ dict/index.d.ts
@@ -1,2 +1,9 @@
+declare function atob(data: string): string;
+declare function btoa(data: string): string;
+declare function clearInterval(handle?: number): void;
+declare function clearTimeout(handle?: number): void;
 declare const core: import('./core').core;
+declare const global: any;
 declare const server: import('./classes').obServer;
+declare function setInterval(handler: string | Function, timeout?: number, ...argumentz: any[]): number;
+declare function setTimeout(handler: string | Function, timeout?: number, ...argumentz: any[]): number;
